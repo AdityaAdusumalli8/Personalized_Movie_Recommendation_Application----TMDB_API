9f723c18da81277998a5505d3f8f881bf9f76673
anushant_singh <anushant888@gmail.com>
2024-12-10 22:57:42 -0600

    moved files to root for heroku

---
+++ server.js
@@ -0,0 +1,23 @@
+const express = require('express');
+const dotenv = require('dotenv');
+const routes = require('./backend/routes');
+const sequelize = require('./backend/config/database');
+
+dotenv.config();
+
+const app = express();
+
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
+app.use('/api', routes);
+
+app.get('/', (req, res) => {
+  res.send('API is running!');
+});
+
+const PORT = process.env.PORT || 3000;
+
+app.listen(PORT, () => {
+  console.log(`Server is running on http://localhost:${PORT}`);
+});
