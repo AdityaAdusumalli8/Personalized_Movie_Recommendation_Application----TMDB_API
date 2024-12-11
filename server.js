cd84b021ba64a8473f2742a1259488cba31f188e
anushant_singh <anushant888@gmail.com>
2024-12-10 23:28:15 -0600

    Configure server to serve React build files

--- server.js
+++ server.js
@@ -2,6 +2,7 @@ const express = require('express');
 const dotenv = require('dotenv');
 const routes = require('./backend/routes');
 const sequelize = require('./backend/config/database');
+const path = require('path');
 
 dotenv.config();
 
@@ -12,8 +13,12 @@ app.use(express.urlencoded({ extended: true }));
 
 app.use('/api', routes);
 
-app.get('/', (req, res) => {
-  res.send('API is running!');
+// Serve static files from the React frontend app
+app.use(express.static(path.join(__dirname, 'frontend/build')));
+
+// Anything that doesn't match the above, send back index.html
+app.get('*', (req, res) => {
+  res.sendFile(path.join(__dirname, 'frontend/build/index.html'));
 });
 
 const PORT = process.env.PORT || 3000;
